e9fa060bb8bb6d40158f1b703356ac2c782513dd
irling <italodavidramosling@gmail.com>
2024-08-21 10:55:56 -0500

    Se llego a completar la funcionalidad de obtencion de registro de llamadas (entrantes, salientes y perdidas)

--- app/src/main/java/com/example/proyectosecurity/CallLogAdapter.kt
+++ app/src/main/java/com/example/proyectosecurity/CallLogAdapter.kt
@@ -1,12 +1,15 @@
 package com.example.proyectosecurity
 
 import android.content.Context
+import android.icu.text.DateFormat
+import android.provider.CallLog
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.LinearLayout
 import android.widget.TextView
+import java.sql.Date
 
 class CallLogAdapter (context: Context, private val callLogs: List<CallLogEntry>)
     :ArrayAdapter<CallLogEntry>(context, 0, callLogs){
@@ -15,15 +18,33 @@ class CallLogAdapter (context: Context, private val callLogs: List<CallLogEntry>
         val view = convertView ?: LayoutInflater.from(context).inflate(R.layout.activity_calls, parent, false)
 
         val itemTemplate = view.findViewById<LinearLayout>(R.id.listItemTemplate)
+        itemTemplate.visibility = View.VISIBLE
 
         val tvNumber = itemTemplate.findViewById<TextView>(R.id.tvNumber)
         val tvType = itemTemplate.findViewById<TextView>(R.id.tvType)
         val tvDate = itemTemplate.findViewById<TextView>(R.id.tvDate)
         val tvDuration = itemTemplate.findViewById<TextView>(R.id.tvDuration)
+        val tvName = itemTemplate.findViewById<TextView>(R.id.tvName)
 
         val callLog = callLogs[position]
 
         tvNumber.text = callLog.number
-        tvType.text = getCallType()
+        tvType.text = getCallType(callLog.type)
+        tvDate.text = DateFormat.getDateInstance().format(Date(callLog.date))
+        tvDuration.text = "${callLog.duration} seconds"
+
+        //MOSTRAR EL NOMBRE SI ESTA DISPONIBLE, SINO MOSTRAR "Nombre desconocido"
+        tvName.text = callLog.contactName ?: "Nombre desconocido"
+
+        return  view
+    }
+
+    private fun getCallType(type: Int): String{
+        return when (type){
+            CallLog.Calls.INCOMING_TYPE -> "Llamada Recibida"
+            CallLog.Calls.OUTGOING_TYPE -> "llamada Realizada"
+            CallLog.Calls.MISSED_TYPE -> "Llamada Perdida"
+            else -> "Desconocido"
+        }
     }
 }
--- app/src/main/java/com/example/proyectosecurity/CallsActivity.kt
+++ app/src/main/java/com/example/proyectosecurity/CallsActivity.kt
@@ -1,7 +1,11 @@
 package com.example.proyectosecurity
 
 import android.content.Context
+import android.database.Cursor
 import android.os.Bundle
+import android.provider.CallLog
+import android.provider.ContactsContract
+import android.widget.ListView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -21,10 +25,76 @@ class CallsActivity : AppCompatActivity() {
     }
 
     private fun setupCallLogListView() {
-
+        val callLogs = getCallLogs(this)
+        val listView = findViewById<ListView>(R.id.listViewCallLogs)
+        val adapter = CallLogAdapter(this, callLogs)
+        listView.adapter = adapter
     }
 
     private fun getCallLogs(context: Context): List<CallLogEntry>{
         val callLogList = mutableListOf<CallLogEntry>()
+
+        val cursor = context.contentResolver.query(
+            CallLog.Calls.CONTENT_URI,
+            null,
+            null,
+            null,
+            CallLog.Calls.DATE + " DESC" //SE ORDENA POR ORDEN DE FECHA DESENDIENTE
+        )
+
+        cursor?.use {
+            val numberColumnIndex = it.getColumnIndex(CallLog.Calls.NUMBER)
+            val typeColumnIndex = it.getColumnIndex(CallLog.Calls.TYPE)
+            val dateColumnsIndex = it.getColumnIndex(CallLog.Calls.DATE)
+            val durationColumnIndex = it.getColumnIndex(CallLog.Calls.DURATION)
+
+            while (it.moveToNext()){
+                val number = it.getString(numberColumnIndex)
+                val type = it.getInt(typeColumnIndex)
+                val date = it.getLong(dateColumnsIndex)
+                val duration = it.getLong(durationColumnIndex)
+                val contactName = getContactName(context, number)
+
+                callLogList.add(
+                    CallLogEntry(
+                        number = number,
+                        type = type,
+                        date = date,
+                        duration = duration,
+                        contactName = contactName
+                    )
+                )
+            }
+        }
+        return callLogList
+    }
+
+    private fun getContactName(context: Context, phoneNumber: String): String? {
+        val projection = arrayOf(ContactsContract.Contacts.DISPLAY_NAME)
+        val cursor = context.contentResolver.query(
+            ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+            projection,
+            "${ContactsContract.CommonDataKinds.Phone.NUMBER} = ?",
+            arrayOf(phoneNumber),
+            null
+        )
+        cursor?.use{
+            if (it.moveToFirst()){
+                val nameIndex = it.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME)
+                return it.getString(nameIndex)
+            }
+        }
+        return null
+    }
+
+    companion object {
+        private const val REQUEST_CODE_READ_CALL_LOG = 100
     }
 }
+data class CallLogEntry(
+    val number : String,
+    val type: Int,
+    val date: Long,
+    val duration: Long,
+    val contactName : String? = null
+)
