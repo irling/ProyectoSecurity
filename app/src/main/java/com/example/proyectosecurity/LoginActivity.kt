f7d3256cb228ac0d9bb8612383cd5706db067df3
irling <italodavidramosling@gmail.com>
2024-09-04 11:21:06 -0500

    integracion de login con parametros temprales de login

--- app/src/main/java/com/example/proyectosecurity/LoginActivity.kt
+++ app/src/main/java/com/example/proyectosecurity/LoginActivity.kt
@@ -1,12 +1,17 @@
 package com.example.proyectosecurity
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class LoginActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +20,25 @@ class LoginActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+        btnLogin.setOnClickListener{accessLogin()}
+    }
+
+    private fun accessLogin() {
+        val etPassword = findViewById<EditText>(R.id.etPassword)
+        val etUsername = findViewById<EditText>(R.id.etUsername)
+
+        val username = etUsername.text.toString()
+        val password = etPassword.text.toString()
+
+
+        if(username == "Admin" && password == "Ciber2024$"){
+            val intentLogin = Intent(this, MainActivity::class.java)
+            startActivity(intentLogin)
+        }else{
+            Toast.makeText(this, "usuario o contraseña incorrecto", Toast.LENGTH_SHORT).show()
         }
     }
 }
